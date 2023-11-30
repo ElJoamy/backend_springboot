e4e64074eb208902a9ee52f44661ae135f87d735
ElJoamy <josephsalguero1@upb.edu>
2023-11-30 12:36:51 -0400

    Add CRUD operations to DepartmentService interface

--- src/main/java/com/upb/projecttwo/services/DepartmentService.java
+++ src/main/java/com/upb/projecttwo/services/DepartmentService.java
@@ -1,8 +1,20 @@
 package com.upb.projecttwo.services;
 
+import java.util.List;
+
 import com.upb.projecttwo.models.Department;
 
 public interface DepartmentService {
     Department save(Department department);
+
+    Department findById(String id);
+
+    List<Department> findAll();
+
+    Department deleteById(String id);
+
+    Department deleteAll();
+
+    Department updateById(String id, Department department);
     
 }
