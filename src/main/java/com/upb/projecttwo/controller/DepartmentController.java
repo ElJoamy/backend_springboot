b0ca9edaf417cf3395b15f51ea05315552055ad3
ElJoamy <josephsalguero1@upb.edu>
2023-11-30 12:37:04 -0400

    Add CRUD operations to DepartmentController

--- src/main/java/com/upb/projecttwo/controller/DepartmentController.java
+++ src/main/java/com/upb/projecttwo/controller/DepartmentController.java
@@ -3,6 +3,8 @@ package com.upb.projecttwo.controller;
 import com.upb.projecttwo.models.Department;
 import com.upb.projecttwo.services.DepartmentService;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,31 @@ public class DepartmentController {
     @PostMapping
     public Department save(@RequestBody Department department) {
         return departmentService.save(department);
+    }
+
+    @GetMapping("/{id}")
+    public Department findById(@PathVariable String id) {
+        return departmentService.findById(id);
+    }
+
+    @GetMapping("/all")
+    public List<Department> findAll() {
+        return departmentService.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public Department deleteById(@PathVariable String id) {
+        return departmentService.deleteById(id);
+    }
+
+    @DeleteMapping("/all")
+    public Department deleteAll() {
+        return departmentService.deleteAll();
+    }
+
+    @PutMapping("/{id}")
+    public Department updateById(@PathVariable String id, @RequestBody Department department) {
+        return departmentService.updateById(id, department);
     }
     
 }
